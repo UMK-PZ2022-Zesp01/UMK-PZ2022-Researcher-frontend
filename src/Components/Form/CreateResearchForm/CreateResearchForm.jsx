4208c19c05340d820943ce245c091766cc852340
RimbiBimbi1 <103674299+RimbiBimbi1@users.noreply.github.com>
2023-03-15 17:44:33 +0100

    S04-T02: Research Creator Rewards
    Great Success!!1

--- src/Components/Form/CreateResearchForm/CreateResearchForm.jsx
+++ src/Components/Form/CreateResearchForm/CreateResearchForm.jsx
@@ -1,31 +1,31 @@
-import React, { useEffect, useState } from "react";
-import "./CreateResearchForm.css";
-import { faFileImage, faTrash, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import getApiUrl from "../../../Common/Api";
-import { CreateResearchFormReward } from "./CreateResearchFormReward";
+import React, { useEffect, useState } from 'react';
+import './CreateResearchForm.css';
+import { faFileImage, faTrash, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import getApiUrl from '../../../Common/Api';
+import { CreateResearchFormReward } from './CreateResearchFormReward';
+import { v4 as uuidv4 } from 'uuid';
 
 function CreateResearchForm() {
-
-  const RESEARCH_ADD_URL = getApiUrl() + "research/add";
-  const PHOTO_UPLOAD_URL = getApiUrl() + "image/upload";
+  const RESEARCH_ADD_URL = getApiUrl() + 'research/add';
+  const PHOTO_UPLOAD_URL = getApiUrl() + 'image/upload';
 
   const loggedUserId = null; //TODO
 
   const [posterImage, setPosterImage] = useState(null);
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [begDate, setBegDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [begDate, setBegDate] = useState('');
+  const [endDate, setEndDate] = useState('');
   const [participantLimit, setParticipantLimit] = useState(0);
-  const [researchForm, setResearchForm] = useState("");
-  const [researchPlace, setResearchPlace] = useState("");
-  const [rewardList, setRewardList] = useState([{ type: "", value: null }]);
-  const [requirementType, setRequirementType] = useState("");
+  const [researchForm, setResearchForm] = useState('');
+  const [researchPlace, setResearchPlace] = useState('');
+  const [rewardList, setRewardList] = useState([{ type: '', value: null }]);
+  const [requirementType, setRequirementType] = useState('');
   const [requirementAgeMin, setRequirementAgeMin] = useState(-1);
   const [requirementAgeMax, setRequirementAgeMax] = useState(-1);
-  const [requirementGender, setRequirementGender] = useState("");
-  const [requirementCustom, setRequirementCustom] = useState("");
+  const [requirementGender, setRequirementGender] = useState('');
+  const [requirementCustom, setRequirementCustom] = useState('');
   const [requirementList, setRequirementList] = useState([]);
 
   let research = {
@@ -35,20 +35,36 @@ function CreateResearchForm() {
     begDate: begDate,
     endDate: endDate,
     participantLimit: participantLimit,
-    location: null
+    location: null,
     // rewards: null
     // requirementList: requirement
   };
 
   /*** Rewards Section ***/
 
+  const [reload, setReload] = React.useState(false);
+
+  useEffect(() => {
+    let newKeyArray = rewardList.reduce(array => [...array, uuidv4()], []);
+    setKeyArray(newKeyArray);
+  }, [reload]);
+
+  const [keyArray, setKeyArray] = React.useState([]);
+
   const renderRewardComponents = () => {
-    return rewardList.length > 0
-      ? rewardList.map((data, index) =>
-        <CreateResearchFormReward key={index.toString()} index={index} data={data}
-                                  handleUpdate={updateReward} handleDelete={deleteReward} />
-      )
-      : <div className="noRewardDesc">W tej chwili Twoje badanie nie oferuje żadnych nagród</div>;
+    return rewardList.length > 0 ? (
+      rewardList.map((data, index) => (
+        <CreateResearchFormReward
+          key={keyArray[index] ? keyArray[index] : 'cfel'}
+          index={index}
+          data={data}
+          handleUpdate={updateReward}
+          handleDelete={deleteReward}
+        />
+      ))
+    ) : (
+      <div className="noRewardDesc">W tej chwili Twoje badanie nie oferuje żadnych nagród</div>
+    );
   };
 
   const updateReward = (index, reward) => {
@@ -57,18 +73,17 @@ function CreateResearchForm() {
     setRewardList(updatedRewardList);
   };
 
-  const deleteReward = (index) => {
-    let updatedRewardList = [...rewardList];
-    updatedRewardList.splice(index, 1);
+  const deleteReward = index => {
+    const leftList = rewardList.slice(0, index);
+    const rightList = rewardList.slice(index + 1);
 
-    console.log("Zwykly:" + rewardList);
-    console.log("Updated:" + updatedRewardList);
-
-    setRewardList(updatedRewardList);
+    setRewardList([...leftList, ...rightList]);
+    setReload(!reload);
   };
 
   const handleAddRewardButtonClick = () => {
-    setRewardList([...rewardList, { type: "", value: null }]);
+    setRewardList([...rewardList, { type: '', value: null }]);
+    setReload(!reload);
   };
 
   useEffect(() => {
@@ -77,7 +92,7 @@ function CreateResearchForm() {
 
   /*** Functions for Handling Changes in Form ***/
 
-  const handlePosterImageChange = (event) => {
+  const handlePosterImageChange = event => {
     setPosterImage(event.target.files[0]);
   };
 
@@ -85,48 +100,48 @@ function CreateResearchForm() {
     setPosterImage(null);
   };
 
-  const handleTitleChange = (event) => {
+  const handleTitleChange = event => {
     setTitle(event.target.value);
     console.log(rewardList);
   };
 
-  const handleDescriptionChange = (event) => {
+  const handleDescriptionChange = event => {
     setDescription(event.target.value);
   };
 
-  const handleBegDateChange = (event) => {
+  const handleBegDateChange = event => {
     setBegDate(event.target.value);
   };
 
-  const handleEndDateChange = (event) => {
+  const handleEndDateChange = event => {
     setEndDate(event.target.value);
   };
 
-  const handleParticipantLimitChange = (event) => {
+  const handleParticipantLimitChange = event => {
     setParticipantLimit(event.target.value);
   };
 
-  const handleResearchFormSelect = (event) => {
+  const handleResearchFormSelect = event => {
     setResearchForm(event.target.value);
   };
 
-  const handleResearchPlaceChange = (event) => {
+  const handleResearchPlaceChange = event => {
     setResearchPlace(event.target.value);
   };
 
   /*** Send New Research to Backend ***/
 
   const addNewResearch = async () => {
-
     /** Photo Upload **/
 
     const formData = new FormData();
-    formData.append("image", posterImage);
-    formData.append("type", "research-poster");
+    formData.append('image', posterImage);
+    formData.append('type', 'research-poster');
 
     try {
       const response = await fetch(PHOTO_UPLOAD_URL, {
-        method: "POST", body: formData
+        method: 'POST',
+        body: formData,
       });
 
       switch (response.status) {
@@ -137,9 +152,11 @@ function CreateResearchForm() {
 
           try {
             const response = await fetch(RESEARCH_ADD_URL, {
-              method: "POST", headers: {
-                "Content-Type": "application/json; charset:UTF-8"
-              }, body: JSON.stringify(research)
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json; charset:UTF-8',
+              },
+              body: JSON.stringify(research),
             });
 
             switch (response.status) {
@@ -160,18 +177,16 @@ function CreateResearchForm() {
           // default
           break;
       }
-
     } catch (e) {
       console.log(e);
     }
-
   };
 
   const handleFormReset = () => {
     resetPosterInput();
   };
 
-  const handleFormSubmit = (event) => {
+  const handleFormSubmit = event => {
     event.preventDefault();
     research.location = { form: researchForm, place: researchPlace };
     addNewResearch().then(null);
@@ -180,86 +195,153 @@ function CreateResearchForm() {
   return (
     <>
       <h2 className="title">Stwórz nowe ogłoszenie o badaniu</h2>
-      <form className="researchForm" onSubmit={handleFormSubmit} onReset={handleFormReset}
-            encType="multipart/form-data">
-
+      <form
+        className="researchForm"
+        onSubmit={handleFormSubmit}
+        onReset={handleFormReset}
+        encType="multipart/form-data"
+      >
         <div className="formRowTop">
-          <input onChange={handlePosterImageChange}
-                 type="file" id="poster" name="poster" accept="image/png, image/jpeg" hidden />
+          <input
+            onChange={handlePosterImageChange}
+            type="file"
+            id="poster"
+            name="poster"
+            accept="image/png, image/jpeg"
+            hidden
+          />
 
           <div className="posterButton">
-            {posterImage != null &&
-              <img alt="poster" src={URL.createObjectURL(posterImage)} className="posterImg" />}
+            {posterImage != null && (
+              <img alt="poster" src={URL.createObjectURL(posterImage)} className="posterImg" />
+            )}
             <div
-              className={posterImage != null ? "posterButtonOverlay" : "posterButtonOverlay overlayActive"}>
-
+              className={
+                posterImage != null ? 'posterButtonOverlay' : 'posterButtonOverlay overlayActive'
+              }
+            >
               <label htmlFor="poster" className="overlayTile">
                 <FontAwesomeIcon className="posterIcon" icon={faFileImage} />
                 <span className="posterButtonDesc">
-                  {posterImage == null ? "Dodaj" : "Zmień"} plakat
+                  {posterImage == null ? 'Dodaj' : 'Zmień'} plakat
                 </span>
               </label>
 
-              {posterImage != null && <div className="overlayTile" onClick={resetPosterInput}>
-                <FontAwesomeIcon className="posterIcon" icon={faTrash} />
-                <span className="posterButtonDesc">Usuń plakat</span>
-              </div>}
-
+              {posterImage != null && (
+                <div className="overlayTile" onClick={resetPosterInput}>
+                  <FontAwesomeIcon className="posterIcon" icon={faTrash} />
+                  <span className="posterButtonDesc">Usuń plakat</span>
+                </div>
+              )}
             </div>
           </div>
 
           <div className="formRow1Right">
-            <input className="formInputRegular" onChange={handleTitleChange}
-                   type="text" id="title" name="title" placeholder="Tytuł badania" required />
-            <textarea className="formInputLarge" onChange={handleDescriptionChange}
-                      id="desc" name="desc" maxLength="1500" placeholder="Opis badania" required />
+            <input
+              className="formInputRegular"
+              onChange={handleTitleChange}
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Tytuł badania"
+              required
+            />
+            <textarea
+              className="formInputLarge"
+              onChange={handleDescriptionChange}
+              id="desc"
+              name="desc"
+              maxLength="1500"
+              placeholder="Opis badania"
+              required
+            />
           </div>
         </div>
 
         <div className="formRow">
           <div className="inputWithLabel">
             <label htmlFor="date-begin">Data rozpoczęcia badania</label>
-            <input className="formInputRegular" onChange={handleBegDateChange}
-                   type="date" id="date-begin" name="date-begin" required />
+            <input
+              className="formInputRegular"
+              onChange={handleBegDateChange}
+              type="date"
+              id="date-begin"
+              name="date-begin"
+              required
+            />
           </div>
 
           <div className="inputWithLabel">
             <label htmlFor="date-end">Data zakończenia badania</label>
-            <input className="formInputRegular" onChange={handleEndDateChange}
-                   type="date" id="date-end" name="date-end" required />
+            <input
+              className="formInputRegular"
+              onChange={handleEndDateChange}
+              type="date"
+              id="date-end"
+              name="date-end"
+              required
+            />
           </div>
 
           <div className="inputWithLabel">
             <label htmlFor="participant-limit">Ilu uczestników potrzebujesz?</label>
-            <input className="formInputRegular" onChange={handleParticipantLimitChange}
-                   type="number" min="0" placeholder="1" id="participant-limit" name="participant-limit" required />
+            <input
+              className="formInputRegular"
+              onChange={handleParticipantLimitChange}
+              type="number"
+              min="0"
+              placeholder="1"
+              id="participant-limit"
+              name="participant-limit"
+              required
+            />
           </div>
         </div>
 
         <div className="rowContainer">
           <label className="formLabel">W jakiej formie przeprowadzasz badanie?</label>
           <div className="formRow">
-            <select onChange={handleResearchFormSelect} className="formInputRegular"
-                    name="form" id="form-select" defaultValue={""} required>
-              <option value="" disabled>Wybierz formę...</option>
+            <select
+              onChange={handleResearchFormSelect}
+              className="formInputRegular"
+              name="form"
+              id="form-select"
+              defaultValue={''}
+              required
+            >
+              <option value="" disabled>
+                Wybierz formę...
+              </option>
               <option value="in-place">stacjonarnie</option>
               <option value="remote">zdalnie</option>
             </select>
-            {researchForm === "remote" &&
-              <input className="formInputRegular" onChange={handleResearchPlaceChange}
-                     type="text" id="remote-link" name="remote-link" placeholder="Link do zdalnego badania" required />}
+            {researchForm === 'remote' && (
+              <input
+                className="formInputRegular"
+                onChange={handleResearchPlaceChange}
+                type="text"
+                id="remote-link"
+                name="remote-link"
+                placeholder="Link do zdalnego badania"
+                required
+              />
+            )}
           </div>
         </div>
-        {researchForm === "in-place" && <div className="formRow">
-          <div className="map">[GOOGLE API MAP]<br />(do wyboru stacjonarnego miejsca badania)</div>
-        </div>}
+        {researchForm === 'in-place' && (
+          <div className="formRow">
+            <div className="map">
+              [GOOGLE API MAP]
+              <br />
+              (do wyboru stacjonarnego miejsca badania)
+            </div>
+          </div>
+        )}
 
         <div className="rowContainer">
           <label className="formLabel">Nagrody za udział w badaniu</label>
 
-          <div className="formColumn">
-            {renderRewardComponents()}
-          </div>
+          <div className="formColumn">{renderRewardComponents()}</div>
 
           <div className="formColumnButton">
             <div onClick={handleAddRewardButtonClick} className="addRewardReqLabel">
@@ -267,7 +349,6 @@ function CreateResearchForm() {
               <span>Dodaj nagrodę</span>
             </div>
           </div>
-
         </div>
 
         {/*<div className={styles.formColumn}>*/}
@@ -328,10 +409,13 @@ function CreateResearchForm() {
         {/*</div>*/}
 
         <div className="formRow">
-          <button className="formButton" type="reset">Zacznij od nowa</button>
-          <button className="formButton" type="submit">Dodaj nowe ogłoszenie</button>
+          <button className="formButton" type="reset">
+            Zacznij od nowa
+          </button>
+          <button className="formButton" type="submit">
+            Dodaj nowe ogłoszenie
+          </button>
         </div>
-
       </form>
     </>
   );
--- src/Components/Form/CreateResearchForm/CreateResearchFormReward.jsx
+++ src/Components/Form/CreateResearchForm/CreateResearchFormReward.jsx
@@ -1,47 +1,70 @@
-import "./CreateResearchFormReward.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { v4 as uuidv4 } from 'uuid';
+import React, { useEffect, useRef } from 'react';
+import './CreateResearchFormReward.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function CreateResearchFormReward({ index, data, handleUpdate, handleDelete }) {
-
   const { type, value } = data;
 
   return (
     <div className="rewardRow">
-      <select required className="formInputRegular" id={uuidv4()} name="reward-type" defaultValue={type}
-              onChange={event => handleUpdate(index, { type: event.target.value, value: null })}>
-        <option value="" disabled>Wybierz typ nagrody...</option>
+      <select
+        required
+        className="formInputRegular"
+        name="reward-type"
+        defaultValue={type}
+        onChange={event => handleUpdate(index, { type: event.target.value, value: null })}
+      >
+        <option value="" disabled>
+          Wybierz typ nagrody...
+        </option>
         <option value="cash">pieniężna</option>
         <option value="item">przedmiot / upominek</option>
       </select>
 
-      {type === "cash" &&
-        <input required className="formInputRegular" type="number" min="0" step="0.01"
-               id={uuidv4()} name="reward-value" placeholder="Kwota w zł" defaultValue={value}
-               onChange={event => handleUpdate(
-                 index, {
-                   type: type,
-                   value: Number(event.target.value)
-                 }
-               )}
+      {type === 'cash' && (
+        <input
+          required
+          className="formInputRegular"
+          type="number"
+          min="0"
+          step="0.01"
+          name="reward-value"
+          placeholder="Kwota w zł"
+          defaultValue={value}
+          onChange={event =>
+            handleUpdate(index, {
+              type: type,
+              value: Number(event.target.value),
+            })
+          }
         />
-      }
+      )}
 
-      {type === "item" &&
-        <input required className="formInputRegular" type="text" id={uuidv4()} name="reward-value"
-               placeholder="Nazwa przedmiotu / upominku" defaultValue={value}
-               onChange={event => handleUpdate(
-                 index, {
-                   type: type,
-                   value: event.target.value
-                 }
-               )}
+      {type === 'item' && (
+        <input
+          required
+          className="formInputRegular"
+          type="text"
+          name="reward-value"
+          placeholder="Nazwa przedmiotu / upominku"
+          defaultValue={value}
+          onChange={event =>
+            handleUpdate(index, {
+              type: type,
+              value: event.target.value,
+            })
+          }
         />
-      }
+      )}
 
-      <div className="removeRewardButton" onClick={() => handleDelete(index)}
-           title="Usuń nagrodę">
+      <div
+        className="removeRewardButton"
+        onClick={() => {
+          handleDelete(index);
+        }}
+        title="Usuń nagrodę"
+      >
         <FontAwesomeIcon icon={faTrash} className="trashIcon" />
       </div>
     </div>
@@ -49,7 +72,7 @@ function CreateResearchFormReward({ index, data, handleUpdate, handleDelete }) {
 }
 
 CreateResearchFormReward.defaultProps = {
-  data: { type: "", value: null }
+  data: { type: '', value: null },
 };
 
 export { CreateResearchFormReward };
