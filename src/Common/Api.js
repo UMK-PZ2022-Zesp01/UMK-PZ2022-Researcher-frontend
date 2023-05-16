2f7e6342783f29d0499a8976a6702a65381498db
RimbiBimbi1 <103674299+RimbiBimbi1@users.noreply.github.com>
2023-05-16 19:39:45 +0200

    Changed back the address

--- src/Common/Api.js
+++ src/Common/Api.js
@@ -1,5 +1,5 @@
 export default function getApiUrl() {
     // return 'https://localhost:443/';
-    return 'http://localhost:8080/';
-    // return 'https://34.118.88.72:8080/';
+    // return 'http://localhost:8080/';
+    return 'https://34.118.88.72:8080/';
 }
