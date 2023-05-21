0d4f7131292c90f17a2ed7956870a6e35a3af840
JJJayKob <jjjaykob@gmail.com>
2023-05-21 21:25:54 +0200

    fixes
    fixed the oopsie

--- src/Components/UserPage/UserPage.jsx
+++ src/Components/UserPage/UserPage.jsx
@@ -41,7 +41,7 @@ export default function UserPage(props) {
 
     /*location section*/
     const [locationInput, setLocationInput] = useState('');
-    const [userLocationCoords,setUserLocationCoords]=useState([])
+    const [userLocationCoords, setUserLocationCoords] = useState([])
     const [isClickedLocation, setIsClickedLocation] = useState(false);
 
     /*email section*/
@@ -63,7 +63,7 @@ export default function UserPage(props) {
     const [phoneState, setPhoneState] = useState();
     const [emailState, setEmailState] = useState();
     const [locationState, setLocationState] = useState();
-    const [coords,setCoords]=useState([])
+    const [coords, setCoords] = useState([])
 
     const [alert, setAlert] = React.useState({
         alertOpen: false,
@@ -267,8 +267,8 @@ export default function UserPage(props) {
         handleLocationClick,
         locationInput: locationInput,
         setLocationInput: setLocationInput,
-        userLocationCoords:userLocationCoords,
-        setUserLocationCoords:setUserLocationCoords,
+        userLocationCoords: userLocationCoords,
+        setUserLocationCoords: setUserLocationCoords,
         setPhoneState: setPhoneState,
         setEmailState: setEmailState,
         setLocationState: setLocationState,
@@ -278,122 +278,39 @@ export default function UserPage(props) {
     return (
         !isLoading && (
             <div className={styles.PageOverlay}>
-            <div className={styles.alertOverlay}>
-                <Popup enabled={alert.alertOpen}>{showAlert()}</Popup>
-            </div>
-            <ReportForm setAlert={setAlert} open={openPopup} onClose={() => setOpenPopup(false)}/>
-            <div className={styles.MainContainer}>
-                <HelmetProvider>
-                    <Helmet>
-                        <title>
-                            {userData.firstName + ' ' + userData.lastName + ' | JustResearch'}
-                        </title>
-                    </Helmet>
-                </HelmetProvider>
-                <div className={styles.UserBox}>
-                    <div className={styles.Container}>
-                        <header className={styles.bookmarksContainer}>
-                            <Link to="/" className={styles.logo}>
-                                <img
-                                    className={styles.logoImg}
-                                    src={researcherLogo}
-                                    alt="Researcher Logo"
-                                />
-                            </Link>
-                            <BookmarksNav active="profile" desc="Twój profil"/>
-                        </header>
-                        <div className={styles.wrapper}>
-                            <div
-                                className={
-                                    clickedResearches
-                                        ? styles.userResearches
-                                        : styles.userResearchesHide
-                                }
-                            >
-                                <div className={styles.userResearchesToggles}>
-                                    <button
-                                        className={styles.exitResBtn}
-                                        onClick={() => setIsClickedResearches(false)}
-                                    >
-                                        <FontAwesomeIcon
-                                            className={styles.arrowIcon}
-                                            icon={faArrowTurnDown}
-                                        />
-                                    </button>
-                                    <div className={styles.inputWrapper}>
-                                        <label className={styles.switch}>
-                                            <input type="checkbox"
-                                                   className={styles.checkbox}
-                                                   id={"checkbox"}
-                                                   checked={fetchCreatedPosts}
-                                                   onChange={(e) => setFetchCreatedPosts(e.target.checked)}/>
-                                            <div className={styles.slider}/>
-                                        </label>
-                                    </div>
-                                </div>
-                                {researchesHeader()}
-                                {fetchCreatedPosts === true && (
-                                    <div className={styles.userResearchCard}>
-                                        {showPostsCreated()}
-                                    </div>
-                                )}
-                                {fetchCreatedPosts === false && (
-                                    <div className={styles.userResearchCard}>
-                                        {showPostsEnrolled()}
-                                    </div>
-                                )}
-                            </div>
-
-
-                            <LeftContainer values={sendToLeftContainer}/>
-                            <RightContainer values={sendToRightContainer}/>
-                        </div>
-                    </div>
-                    <div className={isClickedLocation ? styles.mapBoxVisible : styles.mapBoxHide}>
-                        <Gmap
-                            latitude={53.015331}
-                            longitude={18.6057}
-                            type={'user-page'}
-                            exit={exit}
-                            setLocationInput={setLocationInput}
-                            setIsClickedLocation={setIsClickedLocation}
-                            setGmapExit={setGmapExit}
-                            setResearchPlace={() => {
-                            }}
-                            setResearchPageAddress={() => {
-                            }}
-                        />
-                    </div>
+                <div className={styles.alertOverlay}>
+                    <Popup enabled={alert.alertOpen}>{showAlert()}</Popup>
                 </div>
-                    <ReportForm setAlert={setAlert} open={openPopup} onClose={() => setOpenPopup(false)} />
-                    <div className={styles.MainContainer}>
-                        <HelmetProvider>
-                            <Helmet>
-                                <title>
-                                    {userData.firstName + ' ' + userData.lastName + ' | JustResearch'}
-                                </title>
-                            </Helmet>
-                        </HelmetProvider>
-                        <div className={styles.UserBox}>
-                            <div className={styles.Container}>
-                                <header className={styles.bookmarksContainer}>
-                                    <Link to="/" className={styles.logo}>
-                                        <img
-                                            className={styles.logoImg}
-                                            src={researcherLogo}
-                                            alt="Researcher Logo"
-                                        />
-                                    </Link>
-                                    <BookmarksNav active="profile" desc="Twój profil" />
-                                </header>
-                                <div className={styles.wrapper}>
-                                    <div
-                                        className={
-                                            clickedResearches
-                                                ? styles.userResearches
-                                                : styles.userResearchesHide
-                                        }
-                                    >
+                <ReportForm setAlert={setAlert} open={openPopup} onClose={() => setOpenPopup(false)}/>
+                <div className={styles.MainContainer}>
+                    <HelmetProvider>
+                        <Helmet>
+                            <title>
+                                {userData.firstName + ' ' + userData.lastName + ' | JustResearch'}
+                            </title>
+                        </Helmet>
+                    </HelmetProvider>
+                    <div className={styles.UserBox}>
+                        <div className={styles.Container}>
+                            <header className={styles.bookmarksContainer}>
+                                <Link to="/" className={styles.logo}>
+                                    <img
+                                        className={styles.logoImg}
+                                        src={researcherLogo}
+                                        alt="Researcher Logo"
+                                    />
+                                </Link>
+                                <BookmarksNav active="profile" desc="Twój profil"/>
+                            </header>
+                            <div className={styles.wrapper}>
+                                <div
+                                    className={
+                                        clickedResearches
+                                            ? styles.userResearches
+                                            : styles.userResearchesHide
+                                    }
+                                >
+                                    <div className={styles.userResearchesToggles}>
                                         <button
                                             className={styles.exitResBtn}
                                             onClick={() => setIsClickedResearches(false)}
@@ -403,33 +320,55 @@ export default function UserPage(props) {
                                                 icon={faArrowTurnDown}
                                             />
                                         </button>
-                                        <div className={styles.userResearchCard}>{showPosts()}</div>
+                                        <div className={styles.inputWrapper}>
+                                            <label className={styles.switch}>
+                                                <input type="checkbox"
+                                                       className={styles.checkbox}
+                                                       id={"checkbox"}
+                                                       checked={fetchCreatedPosts}
+                                                       onChange={(e) => setFetchCreatedPosts(e.target.checked)}/>
+                                                <div className={styles.slider}/>
+                                            </label>
+                                        </div>
                                     </div>
-
-                                    <LeftContainer values={sendToLeftContainer} />
-                                    <RightContainer values={sendToRightContainer} />
+                                    {researchesHeader()}
+                                    {fetchCreatedPosts === true && (
+                                        <div className={styles.userResearchCard}>
+                                            {showPostsCreated()}
+                                        </div>
+                                    )}
+                                    {fetchCreatedPosts === false && (
+                                        <div className={styles.userResearchCard}>
+                                            {showPostsEnrolled()}
+                                        </div>
+                                    )}
                                 </div>
-                            </div>
-                            <div className={isClickedLocation ? styles.mapBoxVisible : styles.mapBoxHide}>
-                                <Gmap
-                                    latitude={coords.length>0?Number(coords[0]):53.015331}
-                                    longitude={coords.length>0?Number(coords[1]):18.6057}
-                                    type={'user-page'}
-                                    exit={exit}
-                                    setLocationInput={setLocationInput}
-                                    setIsClickedLocation={setIsClickedLocation}
-                                    setGmapExit={setGmapExit}
-                                    setResearchPlace={() => {}}
-                                    setResearchPageAddress={() => {}}
-                                    setUserLocationCoords={setUserLocationCoords}
-                                    userLocation={locationState}
-                                />
+                                
+                                <LeftContainer values={sendToLeftContainer}/>
+                                <RightContainer values={sendToRightContainer}/>
                             </div>
                         </div>
-                    </div></div>
-
-        </div>
+                        <div className={isClickedLocation ? styles.mapBoxVisible : styles.mapBoxHide}>
+                            <Gmap
+                                latitude={coords.length > 0 ? Number(coords[0]) : 53.015331}
+                                longitude={coords.length > 0 ? Number(coords[1]) : 18.6057}
+                                type={'user-page'}
+                                exit={exit}
+                                setLocationInput={setLocationInput}
+                                setIsClickedLocation={setIsClickedLocation}
+                                setGmapExit={setGmapExit}
+                                setResearchPlace={() => {
+                                }}
+                                setResearchPageAddress={() => {
+                                }}
+                                setUserLocationCoords={setUserLocationCoords}
+                                userLocation={locationState}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
-        
+
     );
 }
