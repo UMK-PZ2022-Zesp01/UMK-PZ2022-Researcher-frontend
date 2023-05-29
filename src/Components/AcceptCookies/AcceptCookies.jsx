87bb17c3ac5fa1878fc54d3e7e8db4f86b9363c5
JJJayKob <jjjaykob@gmail.com>
2023-05-29 14:10:06 +0200

    added a cookie policy pop-up

---
+++ src/Components/AcceptCookies/AcceptCookies.jsx
@@ -0,0 +1,46 @@
+import styles from './AcceptCookies.module.css';
+import getApiUrl from '../../Common/Api';
+import useAuth from '../../hooks/useAuth';
+
+const EDIT_URL = `${getApiUrl()}user/current/update`;
+
+const AcceptCookies = ({open, onClose}) => {
+    const {accessToken} = useAuth().auth;
+
+    const updateCookies = async () => {
+        try {
+            await fetch(EDIT_URL, {
+                method: 'PUT',
+                headers: {
+                    Authorization: accessToken,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({acceptedCookies: true}),
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return open ? (
+        <div className={styles.overlay}>
+            <div
+                className={styles.popupContainer}
+            >
+                <h1>Ta strona strona wykorzystuje pliki cookie</h1>
+                <h3>cookie cookie cookie cookie cookie cookie</h3>
+                <h3>cookie cookie cookie cookie cookie cookie cookie cookie</h3>
+                <div>
+
+                    <button className={styles.profileBtn} onClick={onClose}>
+                        AKCEPTUJĘ
+                    </button>
+                </div>
+            </div>
+        </div>
+    ) : (
+        <></>
+    );
+};
+
+export {AcceptCookies};
--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -11,6 +11,7 @@ import { LoadingDots } from '../LoadingDots/LoadingDots';
 import { Filters } from './Filters/Filters';
 import { FirstTimeForm } from '../Form/FirstTimeForm/FirstTimeForm';
 import { AddResearchTile } from '../ResearchTile/AddResearchTile';
+import {AcceptCookies} from "../AcceptCookies/AcceptCookies";
 
 const RESEARCHES_URL = getApiUrl() + 'research';
 
@@ -176,6 +177,7 @@ function MainPage() {
     /*first login popup*/
     const [openFirstPopup, setOpenFirstPopup] = useState(false);
 
+
     window.onscroll = () => {
         if (window.innerHeight + window.scrollY + 1 >= triggerRef?.current?.offsetHeight) {
             if (!lastPage && !isLoading) {
@@ -283,6 +285,24 @@ function MainPage() {
     // const cutText = (text, toLength) =>
     //     [...text].length > toLength ? text.substring(0, toLength) : text;
 
+    const [openCookies, setOpenCookies] = useState(false);
+
+    useEffect(() => {
+        const acceptedCookies = localStorage.getItem("acceptedCookies");
+        if (acceptedCookies === "false") {
+            setOpenCookies(true);
+        } else if (acceptedCookies === "true") {
+            return;
+        } else {
+            localStorage.setItem("acceptedCookies", false);
+        }
+    }, []);
+
+    const handleCookiesClicked = () => {
+        setOpenCookies(false);
+        localStorage.setItem("acceptedCookies", true);
+    }
+
     const displayPosts = () => {
         return posts.map((post, index) => (
             <ResearchTile
@@ -297,6 +317,7 @@ function MainPage() {
     return (
         <>
             <div className={styles.pageOverlay}>
+                <AcceptCookies open={openCookies} onClose={() => handleCookiesClicked()}/>
                 <FirstTimeForm open={openFirstPopup} onClose={() => setOpenFirstPopup(false)} />
             </div>
             <div className={styles.mainPage}>
