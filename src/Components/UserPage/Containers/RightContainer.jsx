e5c5e68a7a7e1a2d9f6b833c6ca103fe2e67819b
Konrad Zyra <104624422+Zyrekk@users.noreply.github.com>
2023-05-30 11:59:24 +0200

    phone input fix

--- src/Components/UserPage/Containers/RightContainer.jsx
+++ src/Components/UserPage/Containers/RightContainer.jsx
@@ -289,6 +289,26 @@ const RightContainer = ({values}) => {
         }
     };
 
+    const formatAndSetPhoneInput = e => {
+        const inputVal = e.target.value.replace(/ /g, ""); //remove all the empty spaces in the input
+        let inputNumbersOnly = inputVal.replace(/\D/g, ""); // Get only digits
+
+        if (inputNumbersOnly.length > 9) {
+            //If entered value has a length greater than 16 then take only the first 16 digits
+            inputNumbersOnly = inputNumbersOnly.substr(0, 9);
+        }
+
+        // Get nd array of 4 digits per an element EX: ["4242", "4242", ...]
+        const splits = inputNumbersOnly.match(/.{1,3}/g);
+
+        let spacedNumber = "";
+        if (splits) {
+            spacedNumber = splits.join(" "); // Join all the splits with an empty space
+        }
+
+        setPhoneInput(spacedNumber); // Set the new CC number
+    };
+
 
     return (
         <div className={styles.rightContainer}>
@@ -344,7 +364,7 @@ const RightContainer = ({values}) => {
                                 onClick={event => {
                                     event.stopPropagation();
                                 }}
-                                onChange={handlePhoneChange}
+                                onChange={formatAndSetPhoneInput}
                                 type="text"
                                 value={phoneInput}
                                 placeholder="555 555 555"
